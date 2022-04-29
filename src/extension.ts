9f3f0a53467d807ae9b664f4db992cb22e843536
Aldrin Mathew <admin@aldrinmathew.com>
2022-04-30 01:05:36 +0530

    Added `myt.cursorMove`
    - This command moves the cursor a provided number of lines and
      characters relative to the current position

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,70 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+/**
+ * This method is called when the extension is activated
+ * It is activated the very first time the command is executed 
+ */
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "myt" is now active!');
+	/**
+	 * All custom commands of the extension has to be defined in
+	 * package.json
+	 */
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('myt.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Myt!');
+	/**
+	 * Moves the cursor, a specific line and character count from the
+	 * current position
+	 * 
+	 * Remember that cursor position changes are relative to the current
+	 * position of the cursor
+	 */
+	let moveCursorDisposable = vscode.commands.registerCommand('myt.moveCursor', (args) => {
+		/**
+		 * `line` is the number of lines to move. This can be positive or negative
+		 * `char` is the number of characters to move. This can also be positive or negative
+		 * `isAnchor` means whether to move the start of the active selection
+		 * `isActive` means whether to move the active part of the selection, the end,
+		 *   however the active position can come before anchor
+		 */
+		const line = (args.line ?? 0) as number;
+		const char = (args.char ?? 0) as number;
+		const isAnchor = (args.isAnchor ?? true) as boolean;
+		const isActive = (args.isActive ?? true) as boolean;
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return null;
+		}
+		const createPosition = (pos: vscode.Position): vscode.Position => {
+			const newLineValue = pos.line + line;
+			const newCharValue = pos.character + char;
+			/**
+			 * If the new calculated value for line position is less
+			 * than zero, then the new position will be zero.
+			 * 
+			 * The same applies for the character position too.
+			 */
+			const candidate = pos.translate(
+				(newLineValue < 0) ? -newLineValue : line,
+				(newCharValue < 0) ? -newCharValue : char
+			);
+			/** This provides the adjusted position that is verified to be valid by vscode */
+			return editor.document.validatePosition(candidate);
+		};
+		editor.selections = editor.selections.map(
+			(prev) => new vscode.Selection(
+				/** This moves the position of the beginning of the selection */
+				isAnchor ? createPosition(prev.anchor) : prev.anchor,
+				/** This moves the position of the end of the selection */
+				isActive ? createPosition(prev.active) : prev.active
+			)
+		);
 	});
 
-	context.subscriptions.push(disposable);
+	/**
+	 * These subscriptions will be cleared automatically by vscode
+	 * when the extension is deactivated
+	 */
+	context.subscriptions.push(moveCursorDisposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
